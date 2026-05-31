14041512098d1fa3991b6a4e405118c745a70fd6
Bharathi <rbharathi7995@gmail.com>
2026-05-31 19:58:50 +0530

    add Not Found Page

--- ecommerce-project/src/App.jsx
+++ ecommerce-project/src/App.jsx
@@ -4,7 +4,7 @@ import { HomePage } from './pages/HomePage'
 import { CheckoutPage } from './pages/checkout/CheckoutPage'
 import { OrderPage } from './pages/OrderPage'
 import { TrackingPage } from './pages/TrackingPage'
-
+import {Page404} from './pages/Page404'
 
 function App() {
 
@@ -17,6 +17,7 @@ function App() {
       <Route path="checkout" element={<CheckoutPage />} />
       <Route path="orders" element={<OrderPage />} />
       <Route path="tracking" element={<TrackingPage />} />
+      <Route path="*" element={<Page404 />} />
     </Routes>
    
     
---
+++ ecommerce-project/src/pages/Page404.jsx
@@ -0,0 +1,14 @@
+import {Header} from '../components/Header'
+import './Page404.css'
+
+export function Page404(){
+    return(
+       <>
+       <title>404 Page Not Found</title>
+       <link rel="icon" type="image/svg+xml" href="home-favicon.png" />
+         <Header/>
+        
+         <div className="not-found-message">Data Not Found</div>
+       </> 
+    )
+}
