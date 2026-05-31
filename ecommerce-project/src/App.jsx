65e705d6c0728e47bb6a78d83c5d5a7e8b2d68dc
Bharathi <rbharathi7995@gmail.com>
2026-05-31 16:02:18 +0530

    creader header for CheckoutPage and create checkout folder then applied Link

--- ecommerce-project/src/App.jsx
+++ ecommerce-project/src/App.jsx
@@ -1,7 +1,7 @@
 import {Routes, Route} from 'react-router'
 import './App.css'
 import { HomePage } from './pages/HomePage'
-import { CheckoutPage } from './pages/CheckoutPage'
+import { CheckoutPage } from './pages/checkout/CheckoutPage'
 import { OrderPage } from './pages/OrderPage'
 import { TrackingPage } from './pages/TrackingPage'
 
