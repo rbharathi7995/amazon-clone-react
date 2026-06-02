da091aecf7d306f1131a6845cc18e6f18ba0c757
Bharathi <rbharathi7995@gmail.com>
2026-06-02 16:40:44 +0530

    Get cart from the backend amd display quantity

--- ecommerce-project/src/components/Header.jsx
+++ ecommerce-project/src/components/Header.jsx
@@ -5,8 +5,13 @@ import SearchIcon from '../assets/images/icons/search-icon.png'
 import { NavLink } from 'react-router'
 import './Header.css'
 
-export function Header() {
-  return (
+export function Header({ cart }) {
+
+  let totalQuantity = 0;
+  cart.forEach((cartItem) => {
+    totalQuantity +=cartItem.quantity;
+  });
+    return (
     <>
       <div className="header">
         <div className="left-section">
@@ -34,7 +39,7 @@ export function Header() {
 
           <NavLink className="cart-link header-link" to="/checkout">
             <img className="cart-icon" src={CartIcon} />
-            <div className="cart-quantity">3</div>
+            <div className="cart-quantity">{totalQuantity}</div>
             <div className="cart-text">Cart</div>
           </NavLink>
         </div>
